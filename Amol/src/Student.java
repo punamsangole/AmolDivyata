36f83e6cec22a147f5f5c4b10866a7c044e0e960
punamsangole <131657320+punamsangole@users.noreply.github.com>
2024-01-27 13:32:14 +0530

    added commit

--- Amol/src/Student.java
+++ Amol/src/Student.java
@@ -3,6 +3,10 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println("amol local");
 		System.out.println("divyata main");
+		System.out.println("amol local");
+		System.out.println("divyata main");
+		System.out.println("amol local");
+		System.out.println("divyata main");
 	}
 
 }
